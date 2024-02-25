4e3820e0bdae4f36162cc60827a9c84e1ec0ef93
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-02-25 13:13:26 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,7 +2,7 @@ N,M=map(int,input().split())
 arr=list(map(int,input().split()))
 used=[0]*N
 maxi=-21e8
-def dfs(level,total):
+def dfs(level,total,st):
     global maxi
     if total>M:
         return
@@ -10,10 +10,10 @@ def dfs(level,total):
         if total>maxi:
             maxi=total
         return
-    for i in range(N):
+    for i in range(st+1,N):
         if used[i]==0:
             used[i]=1
-            dfs(level+1,total+arr[i])
+            dfs(level+1,total+arr[i],i)
             used[i]=0
-dfs(0,0)
+dfs(0,0,-1)
 print(maxi)
