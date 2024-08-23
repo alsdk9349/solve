6acc22c4828cd4d5c030de6301c260424c289d4e
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-08-23 09:16:25 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.12 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<s;i++) {
+            sb.append(my_string.charAt(i));
+        }
+        for (int i=e;i>=s;i--) {
+            sb.append(my_string.charAt(i));
+        }
+        for (int i=e+1;i<my_string.length();i++) {
+            sb.append(my_string.charAt(i));
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
