e0b0e4fd75499284577023ea2a478225615a02d4
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-08-24 10:44:16 +0900

    [Silver IV] Title: 보물, Time: 100 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -10,7 +10,7 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int [] A = new int[N];
-        int [] B = new int[N];
+        Integer [] B = new Integer[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
@@ -20,11 +20,11 @@ public class Main {
             B[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(A);
-        Arrays.sort(B);
+        Arrays.sort(B,Collections.reverseOrder());
 
         int res = 0;
         for (int i = 0; i < N; i++) {
-            res += A[i] * B[N-i-1];
+            res += A[i] * B[i];
         }
         System.out.println(res);
     }
