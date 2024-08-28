58eeea14919c9804a351a6e4865914ce4247ad0d
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-08-28 10:25:24 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,14 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        for (int i =0;i<myString.length();i++) {
-            char c = myString.charAt(i);
-            String s = String.valueOf(c);
-            s = s.toUpperCase();
-            sb.append(s);
-        }
-        answer = sb.toString();
+        String answer = myString.toUpperCase();
         return answer;
     }
 }
