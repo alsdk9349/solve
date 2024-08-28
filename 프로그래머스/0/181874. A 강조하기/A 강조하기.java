cbf4366bd80d06de590528885f615f3b07a9cfdd
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-08-28 10:39:43 +0900

    [level 0] Title: A 강조하기, Time: 0.11 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,17 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        for (int i=0;i<myString.length();i++) {
-            char c = myString.charAt(i);
-            if (c=='a' || c=='A') {
-                sb.append("A");
-            } else {
-                String s =String.valueOf(c);
-                sb.append(s.toLowerCase());
-            }
-        }
-        
-        String answer = sb.toString();
+        String answer = myString.toLowerCase().replaceAll("a","A");
         return answer;
     }
 }
