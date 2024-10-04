1e77e9700ee84f9a0897ed8cb20e0961a0074c4a
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-10-04 19:09:23 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "Even";
+        if (num%2==1 || num%2==-1) {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
