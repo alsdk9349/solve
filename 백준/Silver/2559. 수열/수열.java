bf899852fa9b64c3d862c66dc3803fd76b53ec0d
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-10-14 21:55:14 +0900

    [Silver III] Title: 수열, Time: 968 ms, Memory: 23740 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int res = (int)-21e8;
+        for (int i=0;i<N-K+1;i++) {
+            int now = 0;
+            for (int j=0;j<K;j++) {
+                now+=arr[i+j];
+            }
+            if (now>res) {
+                res = now;
+            }
+        }
+        System.out.println(res);
+
+    }
+}
