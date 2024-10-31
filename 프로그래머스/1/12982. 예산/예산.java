c372729c57f33b43c21b3533d3b54d1725f782cb
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-10-31 21:27:48 +0900

    [level 1] Title: 예산, Time: 0.56 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        for (int i=0;i<d.length;i++) {
+            if (budget-d[i]>=0) {
+                budget-=d[i];
+                answer++;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
