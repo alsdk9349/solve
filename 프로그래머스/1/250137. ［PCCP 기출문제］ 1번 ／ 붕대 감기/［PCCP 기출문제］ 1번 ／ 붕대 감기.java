06c801a21192915636e147347bf6745a7d9bfa1c
alsdk9349 <156388712+alsdk9349@users.noreply.github.com>
2024-11-21 13:49:40 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.07 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,38 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int answer = health;
-
-        int len = attacks.length;
-        int attackindex = 0;
-        int attack = attacks[attackindex][0];
-        int cnt = 0;
-
-        for (int i=1;i<attacks[len-1][0]+1;i++) {
-            if (i==attack) {
-                answer-=attacks[attackindex][1];
-                cnt=0;
-                if (answer<=0) {
-                    return -1;
-                }
-                attackindex++;
-                if (attackindex<len) {
-                    attack = attacks[attackindex][0];
-                }
-            } else {
-                cnt++;
-                answer+=bandage[1];
-                if (cnt==bandage[0]) {
-                    answer+=bandage[2];
-                    cnt=0;
-                }
-                if (answer>health) {
-                    answer = health;
-                }
-            }
-        }
+        int answer = 0;
         return answer;
     }
 }
